c515cab64a15179eb3f3eca42d3cbd92d3fbb73f
sajadssa <sajadsaeediazad0007@gmail.com>
2025-07-18 21:18:34 +0330

    Remove node_modules from Git tracking

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      {/* first component define BrowserRouter */}
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
+     
+    </>
+  )
+}
+
+export default App
