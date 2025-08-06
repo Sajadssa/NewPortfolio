82e448aa49849201df5e41e0c8129f4c22f0d60b
sajadssa <sajadsaeediazad0007@gmail.com>
2025-08-06 23:39:58 +0330

    Project section...

--- vite-project/src/components/ProjectSection.jsx
+++ vite-project/src/components/ProjectSection.jsx
@@ -1,13 +1,55 @@
 const projects=[
     {
 
-        id:"Project 1",
-        title:"",
-        description:"",
-        image:"",
+        id:1,
+        title:"Portfolio",
+        description:"A project that I made using ReactJS",
+        image:"/vite-project/src/assets/images/Screenshot 2025-08-06 190411.png",
+        tags:["ReactJS","TailwindCSS","ViteJS"],
         link:"",
-        github:"https://github.com/sajad-123456789"
+        github:"https://github.com/Sajadssa/NewPortfolio.git"
+    },
+    {
+
+        id:2,
+        title:"Attary",
+        description:"A project about a web application that I made using javascript",
+        image:"/vite-project/src/assets/images/Screenshot 2025-08-06 191151.png",
+        tags:["HTML/CSS","Javascript","VueJS"],
+        link:"https://pharmacology2022.netlify.app/",
+        github:"https://github.com/Sajadssa/pharmacology.git"
     }
+    ,
+ {
+
+        id:3,
+        title:"online_ticket_purchase",
+        description:"A project about a website that buy tickets online and simulation Wallet",
+        image:"/vite-project/src/assets/images/Screenshot 2025-08-06 191151.png",
+        tags:["PHP","Hack","JavaScript","MySQL"],
+        link:"Need to Apache Sever",
+        github:"https://github.com/Sajadssa/online_ticket_purchase.git"
+    },
+{
+
+        id:4,
+        title:"Language_Schools",
+        description:"A project about language schools that user can learn different languages",
+        image:"/vite-project/src/assets/images/Online_Ticket.png",
+        tags:["PHP","JavaScript","MySQL"],
+        link:"Need to Apache Sever",
+        github:"https://github.com/Sajadssa/Language_Schools.git"
+    },
+    {
+
+        id:5,
+        title:"Language_Schools",
+        description:"A project about language schools that user can learn different languages",
+        image:"/vite-project/src/assets/images/IPAMS.png",
+        tags:["C#","SQL Server",".NET Framework"],
+        link:"Need to Run SQL Server",
+        github:"https://github.com/Sajadssa/Intelligent-Patient-Admission-Management-System.git"
+    },
     
 ]
 
