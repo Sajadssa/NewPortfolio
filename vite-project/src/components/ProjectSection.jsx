95df84964c055de1a0c65fcf242edb446ffaa033
sajadssa <sajadsaeediazad0007@gmail.com>
2025-08-10 00:30:43 +0330

    Project Section is Completed

--- vite-project/src/components/ProjectSection.jsx
+++ vite-project/src/components/ProjectSection.jsx
@@ -3,61 +3,84 @@ import OnlineTicket from "../assets/images/Online_Ticket.png";
 import portfolio from "../assets/images/portfolio.png";
 import attary from "../assets/images/Attary.png";
 import ipams from "../assets/images/IPAMS.png";
-const projects=[
+import Resume from "../assets/images/Resume.png";
+import social from "../assets/images/social.png";
+import { ArrowRight, ExternalLink, GithubIcon } from "lucide-react";
+const projects = [
     {
 
-        id:1,
-        title:"Portfolio",
-        description:"A project that I made using ReactJS",
-        image:portfolio,
-        tags:["ReactJS","TailwindCSS","ViteJS"],
-        link:"",
-        github:"https://github.com/Sajadssa/NewPortfolio.git"
+        id: 1,
+        title: "Portfolio",
+        description: "A project that I made using ReactJS",
+        image: portfolio,
+        tags: ["ReactJS", "TailwindCSS", "ViteJS"],
+        link: "",
+        github: "https://github.com/Sajadssa/NewPortfolio.git"
     },
     {
 
-        id:2,
-        title:"Attary",
-        description:"A project about a web application that I made using javascript",
-        image:attary,
-        tags:["HTML/CSS","Javascript","VueJS"],
-        link:"https://pharmacology2022.netlify.app/",
-        github:"https://github.com/Sajadssa/pharmacology.git"
+        id: 2,
+        title: "Attary",
+        description: "A project about a web application that I made using javascript",
+        image: attary,
+        tags: ["HTML/CSS", "Javascript", "VueJS"],
+        link: "https://pharmacology2022.netlify.app/",
+        github: "https://github.com/Sajadssa/pharmacology.git"
     }
     ,
- {
+    {
 
-        id:3,
-        title:"online_ticket_purchase",
-        description:"A project about a website that buy tickets online and simulation Wallet",
-        image:OnlineTicket,
-        tags:["PHP","Hack","JavaScript","MySQL"],
-        link:"Need to Apache Sever",
-        github:"https://github.com/Sajadssa/online_ticket_purchase.git"
+        id: 3,
+        title: "Online Ticket Purchase",
+        description: "A project about a website that buy tickets online and simulation Wallet",
+        image: OnlineTicket,
+        tags: ["PHP", "Hack", "JavaScript", "MySQL"],
+        link: "Need to Apache Sever",
+        github: "https://github.com/Sajadssa/online_ticket_purchase.git"
     },
 
     {
 
-        id:4,
-        title:"Intelligent-Patient-Admission-Management-System",
-        description:"A project about a system that helps doctors to admit patients and manage their ",
-        image:ipams,
-        tags:["C#","SQL Server",".NET Framework"],
-        link:"Need to Run SQL Server ",
-        github:"https://github.com/Sajadssa/Intelligent-Patient-Admission-Management-System.git"
+        id: 4,
+        title: "Medical Integrated Management System",
+        description: "A project about a system that helps doctors to admit patients and manage their ",
+        image: ipams,
+        tags: ["C#", "SQL Server", ".NET Framework"],
+        link: "Need to Run SQL Server ",
+        github: "https://github.com/Sajadssa/Intelligent-Patient-Admission-Management-System.git"
     },
-    
+    {
+
+        id: 5,
+        title: "Sajad Resume",
+        description: "a project about resume of my and myskills and experiences and projects and achievements",
+        image: Resume,
+        tags: ["HTML/CSS", "Javascript", "VueJS"],
+        link: "https://sajadsaeediazadresume.netlify.app/",
+        github: "https://github.com/Sajadssa/responsive-portfolio-website-Sajad-main.git"
+    },
+
+    {
+
+        id: 6,
+        title: "Social Media Clone",
+        description: "A project about a system that helps doctors to admit patients and manage their ",
+        image: social,
+        tags: ["HTML/CSS", "Javascript", "VueJS"],
+        link: "#",
+        github: "https://github.com/Sajadssa/Responsive-Social-media.git"
+    },
 ]
 
 
 
 
-export const ProjectSection=()=>{
-    return(
+export const ProjectSection = () => {
+    return (
         <section id="projects" className="py-24 px-4 relative">
-<div className="container mx-auto max-w-5xl">
+            <div className="container mx-auto max-w-5xl">
 
-</div>
+            </div>
 
 
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Featured <span className="text-purple-500">Projects</span></h2>
@@ -68,24 +91,48 @@ export const ProjectSection=()=>{
 
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {projects.map((project,key)=>(
-<div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs shadow-neutral-800 card-hover">
-<div className="h-48 overflow-hidden">
-<img
- src={project.image}
-alt={project.title}
-className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
-</div>
-<div className="p-6">
-<div></div>
-</div>
+                {projects.map((project, key) => (
+                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs ">
+                        <div className="h-48 overflow-hidden">
+                            <img
+                                src={project.image}
+                                alt={project.title}
+                                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+                        </div>
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4 items-center justify-center ">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-secondary text-secondary-foreground ">
 
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                        <h3 className=" text-xl font-semibold mb-2 ">{project.title}</h3>
+                        <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
 
+                        <div className="flex justify-between items-center">
+                            <div className="flex space-x-3 ml-2 mb-2">
+                                <a href={project.link} target="_blank" className="text-foreground/80 hover:text-purple-500/80 transition-colors duration-300">
 
-</div>
-
+                                    <ExternalLink size={20} />
+                                </a>
+                                <a href={project.github} target="_blank" className="text-foreground/80 hover:text-purple-500/80 transition-colors duration-300">
+                                    <GithubIcon size={20} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
+<div className="text-center mt-12" >
+    <a> 
+Check out My Github <ArrowRight />
+    </a>
+</div>
+
+            
         </section>
     )
 }
