c8f79c01872b004cecfcfd5230526a730ea939c2
sajadssa <sajadsaeediazad0007@gmail.com>
2025-08-10 00:36:47 +0330

    Modify Navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ export const Navbar = () => {
    className={cn(
     "fixed w-full z-40 transition-all duration-300",isScrolled ?"py-3 bg-background/80 backdrop-blur-md shadow-xs":"py-5")}>
 
-<div className="container flex items-center justify-between">
+<div className="container flex items-center justify-between mr-10 space-x-2">
 <a className="text-xl font-bold text-primary flex items-center" href="#hero">  
 <span className="relative z-10">
 <span className="text-glow text-foreground">SSA</span>
@@ -64,7 +64,7 @@ export const Navbar = () => {
   {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
 </button>
 
-<div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40  flex flex-col items-center justify-center ","transition-all duration-300 md:hidden",isMenuOpen ? "opacity-100 pointer-events-auto":"opacity-0 pointer-events-none")}>
+<div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40  flex flex-col items-center justify-center  ","transition-all duration-300 md:hidden",isMenuOpen ? "opacity-100 pointer-events-auto":"opacity-0 pointer-events-none")}>
 <div className="flex flex-col space-y-8 text-xl">
   
 {navItems.map((item,key)=>(
